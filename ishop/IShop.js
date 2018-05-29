90fa84a023df07938aeb2570f714239b521c0388
Yurkevich, Irina <i.yurkevich@itransition.com>
2018-05-29 16:22:15 +0300

    fd3-iy: #1 iShop - updated title

--- ishop/IShop.js
+++ ishop/IShop.js
@@ -9,27 +9,27 @@ var IShop = React.createClass({
                 React.DOM.th(null, h.imageHead),
                 React.DOM.th(null, h.nameHead),
                 React.DOM.th(null, h.countsHead),
-                React.DOM.th(null, h.priceHead),
+                React.DOM.th(null, h.priceHead)
             )
         );      
 
         var goodsList = this.props.goods.map(v =>
             React.DOM.tr({ key: v.id, className: null},
                 React.DOM.td({className: 'img'},
-                    React.DOM.img({ className: null, src: v.image, height: 60 }),
+                    React.DOM.img({ className: null, src: v.image, height: 60 })
                 ),
                 React.DOM.td({ className: 'name' }, v.name),
                 React.DOM.td(null, v.counts + ' шт.'),
-                React.DOM.td({ className: 'price' }, v.price + ' BYN'),
+                React.DOM.td({ className: 'price' }, v.price + ' BYN')
             )
         );
 
 
         return React.DOM.div(null,
-            React.DOM.h1(null, 'iStore'),
+            React.DOM.h1(null, 'iShop'),
             React.DOM.table({ className: 'good-list' },
                 React.DOM.thead(null,goodsListHead),
-                React.DOM.tbody(null,goodsList),
+                React.DOM.tbody(null,goodsList)
             )
         );
     }
