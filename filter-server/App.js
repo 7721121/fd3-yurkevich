a9ac546d4ef9ea93020d5602a91fbdf3f4859956
Yurkevich, Irina <7721121@tut.by>
2018-06-11 16:02:07 +0300

    fd3-iy: #2 Filter - added server

---
+++ filter-server/App.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import FilterAndSort from './components/FilterAndSort';
+
+let defSorted=false;
+let streetsArr=require('./streets.json');
+
+ReactDOM.render(
+  <FilterAndSort
+    streets={streetsArr}
+    defsorted={defSorted}
+  />
+  , document.getElementById('container') 
+);
+
---
+++ filter-server/components/FilterAndSort.js
@@ -0,0 +1,49 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './styles/FilterAndSort.css';
+
+class FilterAndSort extends React.Component {
+
+  static propTypes = {
+    streets: PropTypes.array,
+    defsorted: PropTypes.bool,
+  };
+
+  state = {
+    isSorted: this.props.defsorted
+  }
+
+  checkboxChangeState = () => {
+      this.setState( {isSorted: !this.state.isSorted} );
+  }
+
+  render() {
+
+  if (this.state.isSorted){
+    alert('sdsf');
+  }else{
+    var streetsList=this.props.streets.map( v =>
+        <li key={v.code}>{v.name}</li>
+    );
+  }
+
+
+    return (
+      <div className='FilterAndSort'>
+        <h1>Filter</h1>
+        <h2>Central District Streets</h2>
+        <label>
+          <input type="checkbox" onChange={this.checkboxChangeState} />
+          Sort It!
+        </label>
+        <ul className='street-list'>{streetsList}</ul>
+      </div>
+    )
+    ;
+
+  }
+
+}
+
+export default FilterAndSort;
