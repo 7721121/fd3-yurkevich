49fff114f5be8fc0624dc4207c4434b75896dc91
Yurkevich, Irina <7721121@tut.by>
2018-06-12 14:00:31 +0300

    fd3-iy: #2 Filter - added for checkbox sorting

--- filter-server/components/FilterAndSort.js
+++ filter-server/components/FilterAndSort.js
@@ -11,39 +11,43 @@ class FilterAndSort extends React.Component {
   };
 
   state = {
-    isSorted: this.props.defsorted
+    isSorted: this.props.defsorted,
+    streetsForSort: this.props.streets
   }
 
   checkboxChangeState = () => {
+
       this.setState( {isSorted: !this.state.isSorted} );
+
+      if (this.state.isSorted === false){
+        var sortedStreets = this.state.streetsForSort.slice(0);
+        sortedStreets.sort(function(a,b) {
+          return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+        });
+        this.setState( {streetsForSort: sortedStreets} );
+
+      }else{
+        this.setState( {streetsForSort: this.props.streets} );
+      }
   }
 
   render() {
 
-  if (this.state.isSorted){
-    alert('sdsf');
-  }else{
-    var streetsList=this.props.streets.map( v =>
-        <li key={v.code}>{v.name}</li>
-    );
-  }
-
-
+  var streetsList = this.state.streetsForSort.map( v =>
+      <li key={v.id}>{v.name}</li>
+  );
     return (
       <div className='FilterAndSort'>
         <h1>Filter</h1>
         <h2>Central District Streets</h2>
-        <label>
-          <input type="checkbox" onChange={this.checkboxChangeState} />
-          Sort It!
-        </label>
+        <div className="checkbox-block">
+          <input type="checkbox" id="sort" onChange={this.checkboxChangeState} />
+          <label htmlFor="sort">Sort It!</label>
+        </div>
         <ul className='street-list'>{streetsList}</ul>
       </div>
-    )
-    ;
-
+    );
   }
-
 }
 
 export default FilterAndSort;
