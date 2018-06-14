90cfde6c32600222b7df47dc43b84c53f6f3dc1f
Yurkevich, Irina <7721121@tut.by>
2018-06-14 16:17:31 +0300

    fd3-iy: #2 IShop2 - added

---
+++ ishop-edited/App.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import IShopEdited from './components/IShopEdited';
+
+let goodsHeadArr = [
+  { nameHead: 'Наименование', priceHead: 'Цена', imageHead: 'Изображение', countsHead: 'В наличии' }
+];
+let goodsArr=require('./goods.json');
+
+ReactDOM.render(
+  <IShopEdited
+    goods={goodsArr}
+    goodsHead={goodsHeadArr}
+  />
+  , document.getElementById('container') 
+);
+
---
+++ ishop-edited/components/IShopEdited.js
@@ -0,0 +1,63 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './styles/IShopEdited.css';
+
+class IShopEdited extends React.Component {
+
+  static propTypes:{
+    goodsHead: PropTypes.array,
+    goods: PropTypes.array,
+/*    goods: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        image: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        count: PropTypes.number,
+        price: PropTypes.number
+      })*/
+  }
+
+  state = {
+    activetRow: false,
+    class: ''
+  };
+
+  hightlightRow = (e) => {
+    console.log(e.target);
+      //e.preventDefault();
+      this.setState({activetRow: !this.state.activetRow, class: 'active'});
+  }
+
+  render() {
+
+  const goodsListHead = this.props.goodsHead.map(h =>
+    <tr key={h.nameHead}>
+      <th>{h.imageHead}</th>
+      <th>{h.nameHead}</th>
+      <th>{h.countsHead}</th>
+      <th>{h.priceHead}</th>
+    </tr>
+  );
+
+  const goodsList = this.props.goods.map(v =>
+    <tr key={v.id} onClick={this.hightlightRow} className={this.state.class}>
+      <td className="img"><img src={v.image} height="60" /></td>
+      <td className="name">{v.name}</td>
+      <td>{v.counts}</td>
+      <td className="price">{v.price + ' BYN'}</td>
+    </tr>
+  );
+
+    return (
+      <div className="goodsTable">
+        <h1>iShop</h1>
+        <table className="good-list">
+          <thead>{goodsListHead}</thead>
+          <tbody>{goodsList}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default IShopEdited;
