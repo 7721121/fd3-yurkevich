0bb92f9fdae8d4485e649663b9218013931c47bf
Yurkevich, Irina <7721121@tut.by>
2018-06-15 16:03:00 +0300

    fd3-iy: #3 IShop2 - added onClick

--- ishop-edited/components/IShopEdited.js
+++ ishop-edited/components/IShopEdited.js
@@ -18,43 +18,81 @@ class IShopEdited extends React.Component {
   }
 
   state = {
-    activetRow: false,
-    class: ''
+    goodID: null,
+    openCard: false
   };
 
   hightlightRow = (e) => {
-    console.log(e.target);
-      //e.preventDefault();
-      this.setState({activetRow: !this.state.activetRow, class: 'active'});
+    let target = e.target;
+    let targetRow = target.closest('tr');
+    let targetTable = target.closest('.goods-table-list');
+
+  
+    if(targetRow.classList.contains('active')){
+      targetRow.classList.remove('active');
+      let targetRowID = targetRow.setAttribute('data-id','');
+      this.setState({openCard: false, goodID: null });
+    }else{
+      let selectedRows = targetTable.querySelectorAll('.active');
+      if(selectedRows.length!=0){
+        selectedRows.forEach(row => {
+          if (row.classList.contains('active')) {
+            row.classList.remove('active');
+          }
+        });
+      }
+      targetRow.classList.add('active');
+      let targetRowID = targetRow.getAttribute('data-id');
+      this.setState({openCard: !this.state.activetRow, goodID: targetRowID });
+    }
   }
 
   render() {
 
-  const goodsListHead = this.props.goodsHead.map(h =>
-    <tr key={h.nameHead}>
-      <th>{h.imageHead}</th>
-      <th>{h.nameHead}</th>
-      <th>{h.countsHead}</th>
-      <th>{h.priceHead}</th>
-    </tr>
-  );
+    const goodsListHead = this.props.goodsHead.map(h =>
+      <tr key={h.nameHead}>
+        <th>{h.imageHead}</th>
+        <th>{h.nameHead}</th>
+        <th>{h.countsHead}</th>
+        <th>{h.priceHead}</th>
+      </tr>
+    );
 
-  const goodsList = this.props.goods.map(v =>
-    <tr key={v.id} onClick={this.hightlightRow} className={this.state.class}>
-      <td className="img"><img src={v.image} height="60" /></td>
-      <td className="name">{v.name}</td>
-      <td>{v.counts}</td>
-      <td className="price">{v.price + ' BYN'}</td>
-    </tr>
-  );
+    const goodsList = this.props.goods.map(v =>
+      <tr key={v.id} data-id={v.id}>
+        <td className="img"><img src={v.image} height="60" /></td>
+        <td className="name">{v.name}</td>
+        <td>{v.counts}</td>
+        <td className="price">{v.price + ' BYN'}</td>
+      </tr>
+    );
+    const activeCard = this.props.goods.map(item =>
+      <div key={item.id}>
+        <div className="goods-card-title">{item.name}</div>
+        <div className="goods-card-descr">
+          <div className="goods-card-image"><img src={item.image} alt={item.name} /></div>
+          <div><b>{'В наличии: '}</b>{item.counts + 'шт.'}</div>
+          <div><h1 className="right">{'Цена: ' + item.price + ' BYN'}</h1></div>
+          <button className="btn btn-edit right">Редактировать</button>
+        </div>
+      </div>
+    );
 
     return (
-      <div className="goodsTable">
+      <div>
         <h1>iShop</h1>
-        <table className="good-list">
-          <thead>{goodsListHead}</thead>
-          <tbody>{goodsList}</tbody>
-        </table>
+        <div className="goods-table-wr">
+        <div className={"goods-table"+((this.state.goodID!=null) ? " open" : "")}>
+          <table className="goods-table-list" onClick={this.hightlightRow}>
+            <thead>{goodsListHead}</thead>
+            <tbody>{goodsList}</tbody>
+          </table>
+        </div>
+
+        <div className={"goods-card"+((this.state.goodID!=null) ? " show" : "")}>
+            {activeCard[this.state.goodID-1]}
+        </div>
+        </div>
       </div>
     );
   }
